84c5c0d74f690fe126f8b0bf89dc1613cbe06cad
GabriielFortes <gabriielfortes@gmail.com>
2025-04-25 06:31:00 -0400

    Developing_Part_3

---
+++ Emotion_Detection/emotion_detection.py
@@ -0,0 +1,33 @@
+'''
+This module uses the embeddable Watson AI libraries to create an emotion detection application.
+Emotion detection extends the concept of sentiment analysis by extracting more subtle emotions, 
+such as happiness, sadness, anger, and so on, from statements rather than the simple polarity 
+that sentiment analysis provides.
+'''
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    '''
+    The function receives a parameter of type string and sends a 
+    request to the Watson AI to analyze the string's emotions.
+    '''
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/"
+        "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+        )
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    dict_emotion = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json=dict_emotion, headers=header, timeout=10)
+    response_format = json.loads(response.text)
+
+    emotions = response_format["emotionPredictions"][0]["emotion"]
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"]
+    }
